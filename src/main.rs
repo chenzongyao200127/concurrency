b9b440444750623c9f0c7671ba0ff2e17ff67cbd
chenzongyao200127 <2319251792@qq.com>
2024-04-26 17:45:24 +0800

    feature: support sequential matrix multiply

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use concurrency::{multiply, Matrix};
+
+fn main() -> Result<()> {
+    let a = Matrix::new(vec![1, 2, 3, 4, 5, 6], 2, 3);
+    let b = Matrix::new(vec![1, 2, 3, 4, 5, 6], 3, 2);
+    println!("{:?}", a);
+    println!("{:?}", b);
+    let c = multiply(&a, &b)?;
+    println!("{:?}", c);
+    Ok(())
 }
