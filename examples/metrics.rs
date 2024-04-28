01bd62fbb295cc42e7f8b3bcb436e77b8fb1389c
chenzongyao200127 <2319251792@qq.com>
2024-04-28 19:57:28 +0800

    feature: support Rwlock and Display

--- examples/metrics.rs
+++ examples/metrics.rs
@@ -24,7 +24,7 @@ fn main() -> Result<()> {
 
     loop {
         thread::sleep(std::time::Duration::from_secs(1));
-        println!("{:?}", metrics.snapshot()?);
+        println!("{}", metrics);
     }
 
     // Ok(())
