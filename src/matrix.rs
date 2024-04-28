88d7e718753a351f8d3b847cfd948d008462d87a
chenzongyao200127 <2319251792@qq.com>
2024-04-28 16:32:31 +0800

    refactor: make matrix muliply to be concurreny with threads

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,10 +1,14 @@
+use crate::{dot_product, Vector};
+use anyhow::{anyhow, Result};
 use core::fmt;
 use std::{
     fmt::Debug,
     ops::{Add, AddAssign, Mul},
+    sync::mpsc,
+    thread,
 };
 
-use anyhow::{anyhow, Result};
+const NUM_THREAD: usize = 4;
 
 // martix example:
 // [[1, 2], [1, 2], [1, 2]] => [1, 2, 1, 2, 1, 2]
@@ -15,23 +19,87 @@ pub struct Matrix<T> {
     col: usize,
 }
 
+pub struct MsgInput<T> {
+    idx: usize,
+    row: Vector<T>,
+    col: Vector<T>,
+}
+
+pub struct MsgOutput<T> {
+    idx: usize,
+    result: T,
+}
+
+pub struct Msg<T> {
+    input: MsgInput<T>,
+    // sender to send the result back, use oneshot channel
+    sender: oneshot::Sender<MsgOutput<T>>,
+}
+
 pub fn multiply<T>(a: &Matrix<T>, b: &Matrix<T>) -> Result<Matrix<T>>
 where
-    T: Debug + Copy + Default + Add<Output = T> + AddAssign + Mul<Output = T>,
+    T: Debug + Copy + Default + Add<Output = T> + AddAssign + Mul<Output = T> + Send + 'static,
 {
+    // Check matrix dimensions to ensure they are compatible for multiplication
     if a.col != b.row {
-        return Err(anyhow!("Matrix a's col must be equal to matrix b's row"));
+        return Err(anyhow!(
+            "Matrix a's column count must be equal to matrix b's row count."
+        ));
     }
 
-    let mut data = vec![T::default(); a.row * b.col];
+    // Create channels and spawn worker threads for parallel processing
+    let senders = (0..NUM_THREAD)
+        .map(|_| {
+            let (tx, rx) = mpsc::channel::<Msg<T>>();
+            thread::spawn(move || {
+                for msg in rx {
+                    let value = dot_product(msg.input.row, msg.input.col)?;
+                    if let Err(e) = msg.sender.send(MsgOutput {
+                        idx: msg.input.idx,
+                        result: value,
+                    }) {
+                        eprintln!("Error sending result: {}", e);
+                    }
+                }
+                Ok::<_, anyhow::Error>(())
+            });
+            tx
+        })
+        .collect::<Vec<_>>();
+
+    // Prepare containers for the results and receivers to collect outputs
+    let matrix_len = a.row * b.col;
+    let mut data = vec![T::default(); matrix_len];
+    let mut receivers = Vec::with_capacity(matrix_len);
+
+    // Distribute tasks among workers
     for i in 0..a.row {
         for j in 0..b.col {
-            for k in 0..a.col {
-                data[i * b.col + j] += a.data[i * a.col + k] * b.data[k * b.col + j];
+            let row = Vector::new(&a.data[i * a.col..(i + 1) * a.col]);
+            let col_data = b.data[j..]
+                .iter()
+                .step_by(b.col)
+                .copied()
+                .collect::<Vec<_>>();
+            let col = Vector::new(col_data);
+            let idx = i * b.col + j;
+            let input = MsgInput::new(idx, row, col);
+            let (oneshot_tx, oneshot_rx) = oneshot::channel();
+            let msg = Msg::new(input, oneshot_tx);
+            if let Err(e) = senders[idx % NUM_THREAD].send(msg) {
+                eprintln!("Error dispatching task: {}", e);
             }
+            receivers.push(oneshot_rx);
         }
     }
 
+    // Collect results from worker threads
+    for rx in receivers {
+        let output = rx.recv()?;
+        data[output.idx] = output.result;
+    }
+
+    // Construct the resulting matrix
     Ok(Matrix {
         data,
         row: a.row,
@@ -78,6 +146,29 @@ where
     }
 }
 
+impl<T> MsgInput<T> {
+    pub fn new(idx: usize, row: Vector<T>, col: Vector<T>) -> Self {
+        Self { idx, row, col }
+    }
+}
+
+impl<T> Msg<T> {
+    pub fn new(input: MsgInput<T>, sender: oneshot::Sender<MsgOutput<T>>) -> Self {
+        Self { input, sender }
+    }
+}
+
+impl<T> Mul for Matrix<T>
+where
+    T: Debug + Copy + Default + Add<Output = T> + AddAssign + Mul<Output = T> + Send + 'static,
+{
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        multiply(&self, &rhs).expect("Failed to multiply matrices")
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use anyhow::Ok;
@@ -90,7 +181,7 @@ mod tests {
         let b = Matrix::new(vec![1, 2, 3, 4, 5, 6], 3, 2);
         println!("{:?}", a);
         println!("{:?}", b);
-        let c = multiply(&a, &b)?;
+        let c = a * b;
         println!("{:?}", c);
         assert!(c.data == vec![22, 28, 49, 64]);
         Ok(())
@@ -102,5 +193,21 @@ mod tests {
         println!("{:?}", a);
         assert!(a.data == vec![1, 2, 3, 4, 5, 6]);
         Ok(())
+    }
+
+    #[test]
+    fn test_a_can_not_multiply_b() {
+        let a = Matrix::new(vec![1, 2, 3, 4, 5, 6], 2, 3);
+        let b = Matrix::new(vec![1, 2, 3, 4, 5, 6], 3, 3);
+        let c = multiply(&a, &b);
+        assert!(c.is_err());
+    }
+
+    #[test]
+    #[should_panic]
+    fn text_a_can_not_multiply_b_panic() {
+        let a = Matrix::new(vec![1, 2, 3, 4, 5, 6], 2, 3);
+        let b = Matrix::new(vec![1, 2, 3, 4, 5, 6], 2, 3);
+        let _ = a * b;
     }
 }
