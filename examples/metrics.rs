f2d5dfc4f110f731fc8e3ef267fdd250066d2acc
chenzongyao200127 <2319251792@qq.com>
2024-04-28 19:48:42 +0800

    fix: do not use unwrap

---
+++ examples/metrics.rs
@@ -0,0 +1,62 @@
+use anyhow::Result;
+use concurrency::Metrics;
+use rand::Rng;
+use std::thread;
+
+const N: usize = 2;
+const M: usize = 4;
+
+fn main() -> Result<()> {
+    let metrics = Metrics::new();
+
+    // start N works and M requests
+
+    println!("Start {} workers and {} requests", N, M);
+    println!("{:?}", metrics.snapshot()?);
+
+    for i in 0..N {
+        task_worker(i, metrics.clone())?; // Metrics {data: Arc::clone(&metrics.data)}
+    }
+
+    for _ in 0..M {
+        request_worker(metrics.clone())?;
+    }
+
+    loop {
+        thread::sleep(std::time::Duration::from_secs(1));
+        println!("{:?}", metrics.snapshot()?);
+    }
+
+    // Ok(())
+}
+
+fn task_worker(idx: usize, metrics: Metrics) -> Result<()> {
+    thread::spawn(move || {
+        loop {
+            // do long term stuff
+            let mut rng = rand::thread_rng();
+            thread::sleep(std::time::Duration::from_millis(rng.gen_range(100..666)));
+            let _ = metrics.inc(format!("call.thread.worker.{}", idx));
+        }
+        #[allow(unreachable_code)]
+        Ok::<_, anyhow::Error>(())
+    });
+
+    Ok(())
+}
+
+fn request_worker(metrics: Metrics) -> Result<()> {
+    thread::spawn(move || {
+        loop {
+            // do long term stuff
+            let mut rng = rand::thread_rng();
+            thread::sleep(std::time::Duration::from_millis(rng.gen_range(50..700)));
+            let page = rng.gen_range(1..=256);
+            metrics.inc(format!("req.page.{}", page))?;
+        }
+        #[allow(unreachable_code)]
+        Ok::<_, anyhow::Error>(())
+    });
+
+    Ok(())
+}
